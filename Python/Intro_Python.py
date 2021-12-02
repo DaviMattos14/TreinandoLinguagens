71c96e4c9fd56ee595cb3c2d8dfc1bcda1ce9b48
DaviMattos14 <davimattos2012@gmail.com>
2021-12-02 12:49:30 -0300

    Introdução a python

---
+++ Python/Intro_Python.py
@@ -0,0 +1,45 @@
+#Comentário
+
+print("Hello World") # Imprimir algo na tela
+
+#Variáveis
+
+a = 4
+b = "Dios Mio"
+
+#Especificando variáveis
+
+string = str(14) # '14'
+inteiro = int(14) # 14
+flutuante = float(14) # 14.0
+
+print(type(a)) # saber o tipo da variável
+
+#Defininfo caracteres
+
+nome = "Davi"
+# é igual a
+nome = 'Davi'
+
+#Operadores Booleanos
+
+if 2 < 6 :
+    print("2 menor que 6")
+else:
+    print("2 não é menor que 6")
+
+#Lista
+
+lista = ["1","2","3"]
+
+#Tuplas 
+
+tupla = ("4","5","6")
+
+#Dicionários
+
+dict = {
+    "Davi" : 14,
+    "Lannes" : 1,
+    "Amaral" : 7
+}
