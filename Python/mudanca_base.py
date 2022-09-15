94fdbbc5e57eb1a16eff330a8cbe4ca12d4969d9
Davi Mattos <davimattos2012@gmail.com>
2022-09-15 09:53:40 -0300

    Update mudanca_base.py
    automatizando

--- Python/mudanca_base.py
+++ Python/mudanca_base.py
@@ -8,3 +8,15 @@ while numero > 0:
     numero = int((numero - (numero % 2))/2)
     p += 1
 print(binario)
+
+#------------------------------------------------#
+
+for numero in range(21):
+    n = numero
+    binario = 0
+    p=0
+    while n > 0:
+        binario += ((n % 2) * (pow(10,p)))
+        n = int((n - (n % 2))/2)
+        p += 1
+    print(numero, " = ", binario)
