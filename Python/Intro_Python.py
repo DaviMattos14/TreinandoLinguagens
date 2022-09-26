573f690a504c2e713cbaec8c554d72e301b767c0
DaviMattos14 <davimattos2012@gmail.com>
2022-09-26 12:05:09 -0300

    26/09/2022

--- Python/Intro_Python.py
+++ Python/Intro_Python.py
@@ -1,45 +1,63 @@
-#Comentário
+# Comentário
 
-print("Hello World") # Imprimir algo na tela
+from Python.comp2.veiculo import veiculo
+import unittest
+print("Hello World")  # Imprimir algo na tela
 
-#Variáveis
+# Variáveis
 
 a = 4
 b = "Dios Mio"
 
-#Especificando variáveis
+# Especificando variáveis
 
-string = str(14) # '14'
-inteiro = int(14) # 14
-flutuante = float(14) # 14.0
+string = str(14)  # '14'
+inteiro = int(14)  # 14
+flutuante = float(14)  # 14.0
 
-print(type(a)) # saber o tipo da variável
+print(type(a))  # saber o tipo da variável
 
-#Defininfo caracteres
+# Defininfo caracteres
 
 nome = "Davi"
 # é igual a
 nome = 'Davi'
 
-#Operadores Booleanos
+# Operadores Booleanos
 
-if 2 < 6 :
+if 2 < 6:
     print("2 menor que 6")
 else:
     print("2 não é menor que 6")
 
-#Lista
+# Lista
 
-lista = ["1","2","3"]
+lista = ["1", "2", "3"]
 
-#Tuplas 
+# Tuplas
 
-tupla = ("4","5","6")
+tupla = ("4", "5", "6")
 
-#Dicionários
+# Dicionários
 
 dict = {
-    "Davi" : 14,
-    "Lannes" : 1,
-    "Amaral" : 7
+    "Davi": 14,
+    "Lannes": 1,
+    "Amaral": 7
 }
+
+# Teste Unitário
+
+"""
+(self, marca, modelo, dono, tanque_capacid,tanque_atual, autonomia_km_l, km_rodados)
+"""
+
+
+class TestVeiculo(unittest.TestCase):
+    def test_lerTanqueAtual(self):
+        car1 = veiculo('Fiat', 'Siena', 'Carlos', 80, 0, 5, 0)
+        self.assertEqual(car1.lerTanqueAtual(), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- Python/comp2/Pilha.py
+++ Python/comp2/Pilha.py
@@ -1,17 +1,16 @@
 """
     Nome: Davi dos Santos Mattos    DRE: 119133049
 """
-
 class Pilha:
 
-    def __init__(self, elementos = []): # list → list
+    def __init__(self, elementos=[]):  # list → list
         """
             Método construtor, criar o atributo 'elemento' da classe Pilha,
             recebendo uma lista e retornando uma mensagem de sucesso juntamente com os itens atribuidos,
             caso a parâmetro recebido não seja uma lista,
             ele retorna uma mensagem de erro
         """
-        if(isinstance(elementos, list)==True):
+        if (isinstance(elementos, list) == True):
             self.elementos = elementos
             print(self.elementos)
             print("Sucesso\n")
@@ -20,56 +19,84 @@ class Pilha:
             print(self.elementos)
             print("Erro\n")
 
-    def empilhar(self, elemento): # list → list
+    def __add__(self, other):
+        '''
+            Realiza uma sobrecarga de operadror "+" através do método especial permitindo mesclar dois objetos
+        '''
+        if (isinstance(self, list) == isinstance(other, list)):
+            pilha1 = Pilha(self.getPilha() + other.getPilha())
+            return pilha1
+        else:
+            print('Erro')
+
+    def empilhar(self, elemento):  # list → list
         """
             Adiciona um elemento no final da lista
         """
         self.elementos.append(elemento)
-    
-    def desempilhar(self): # list → list, string
+
+    def desempilhar(self):  # list → list, string
         """
             Remove o elemento no final da lista, 
             caso a lista eseteja vazia ele retona uma mensagem de erro
         """
-        if(len(self.elementos) > 0):
+        if (len(self.elementos) > 0):
             self.elementos.pop(len(self.elementos)-1)
-        elif(len(self.elementos) == 0):
+        elif (len(self.elementos) == 0):
             print("Erro")
 
-    def getPilha(self): # list → list
+    def getPilha(self):  # list → list
         """
             Retorna uma lista
         """
         return self.elementos
-    
-    def lenPilha(self): # list → int
+
+    def lenPilha(self):  # list → int
         """
             Retorna a quantidade de elementos da lista
         """
         return len(self.elementos)
 
 
+if __name__ == '__main__':
+
 # 7.a) Testando método construtor
-p1 = Pilha([1,7,9])
-p2 = Pilha()
+    print('\n7.a)')
+    p1 = Pilha([1, 7, 9])
+    p2 = Pilha()
 
 # 7.b) Testando método empilhar
-p3 = Pilha(10)
+    # A instãncia é criada com o valor padrão, porém é retornado uma mensagem de erro
+    print('\n7.b)')
+    p3 = Pilha(10)
 
 # 7.c) Testando método desempilhar
-p2.empilhar(4)
-p2.empilhar(10)
+    print('\n7.c)')
+    p2.empilhar(4)
+    p2.empilhar(10)
+    print(p2.getPilha())
 
 # 7.d)
-for i in range(len(p2.elementos)+1):
-	p2.getPilha()
-	p2.desempilhar()
+    print('\n7.d)')
+    for i in range(len(p2.elementos)+1):
+        print(p2.getPilha())
+        p2.desempilhar()
 
 # 7.e)
-p2.empilhar(5)
+    print('\n7.e)')
+    p2.empilhar(5)
+    print(p2.getPilha())
 
-# 7.f)
+# 7. f)
+    print('\n7.f)')
+    p1 = Pilha([4,2,9]) 
+    p2 = Pilha([0,3,2])
+    p3 = p2 + p1
+    p4 = p1 + p2
+ 
+
+# 7.g)
+    print('\n7.g)')
+    print(p3.lenPilha())
 
 
-# 7.g) 
-p3.getPilha()
