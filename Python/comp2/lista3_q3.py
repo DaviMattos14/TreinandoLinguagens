91fab570e4d6a51987eb32fca4c37182ab301b7b
DaviMattos14 <davimattos2012@gmail.com>
2022-10-12 12:27:55 -0300

    Lista 3 - Comp2 2022.2

--- Python/comp2/lista3_q3.py
+++
@@ -1,8 +0,0 @@
-media = open('Media.txt', 'r')
-for valor in media:
-    aux = valor.split(' ')
-    aux2 = aux[1].strip()
-    if float(aux2) >= 6:
-        print(aux[0]+' | '+aux2+' | '+'APROVADO')
-    else:
-        print(aux[0]+' | '+aux2+' | '+'REPROVADO')
