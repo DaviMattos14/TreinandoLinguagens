7f00579ff90087cacff8ebe49703c4afa9113a73
DaviMattos14 <davimattos2012@gmail.com>
2022-10-13 19:29:30 -0300

    Lista 4 - Conjuntos

--- Python/comp2/lista5_q3.py
+++
@@ -1,9 +0,0 @@
-def mercado(produtos,lista_compra):
-    disponiveis = set(produtos)
-    lista = set(lista_compra)
-    return lista - disponiveis
-
-if __name__ == '__main__':
-    lista = ['ovo','queijo','presunto','pão']
-    produtos_estoque = ['ovo','queijo','presunto','guaraná','farinha']
-    print(mercado(produtos_estoque,lista))
