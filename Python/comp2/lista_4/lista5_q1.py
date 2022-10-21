6dc4fcf2800de77f2cbcfa42ecda96421e7509c7
DaviMattos14 <davimattos2012@gmail.com>
2022-10-21 15:02:14 -0300

    Lista 4 [Conjuntos] - Questão 1 - Comp2 2022.2

--- Python/comp2/lista_4/lista5_q1.py
+++ Python/comp2/lista_4/lista5_q1.py
@@ -1,7 +1,7 @@
 def penetra(convidados, presentes):
-    if ((set(presentes) - set(convidados)) == set()):
+    if ((set(presentes) - set(convidados)) != set()):
         print('Há intruso na festa\n')
-    elif (((set(presentes) - set(convidados))) != set()):
+    elif (((set(presentes) - set(convidados))) == set()):
         print("Não há penetras\n")
 
 if __name__ == '__main__':
