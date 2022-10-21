f20116c456be9a510f0d95d7e6b71641951c262f
DaviMattos14 <davimattos2012@gmail.com>
2022-10-21 15:02:29 -0300

    Lista 4 [Conjuntos] - Questão 2 - Comp2 2022.2

--- Python/comp2/lista_4/lista5_q2.py
+++ Python/comp2/lista_4/lista5_q2.py
@@ -1,15 +1,15 @@
-def homens40(funcionarios, func_M, func40):
-    f = set(funcionarios)
+def homens40(funcionarios, func_M, funcionarios_40):
+    total_funcionarios = set(funcionarios)
     mulheres = set(func_M)
-    h_40 = set(func40)
-    return (f - mulheres) - h_40
+    func_40_mais = set(funcionarios_40)
+    print((total_funcionarios - mulheres) - func_40_mais)
 
 
 if __name__ == '__main__':
     func = ['Marcos', 'Lucas', 'Mariana', 'Yan', 'Carlos',
             'Larissa', 'Leticia', 'Debora', 'Alana', 'Alan', 'Caio']
     mulheres = ['Mariana', 'Larissa', 'Leticia', 'Debora', 'Alana']
-    homens = ['Marcos', 'Alan', 'Carlos']
+    homens = ['Marcos', 'Alan', 'Carlos', 'Larissa']
     # Lucas, Yan, Caio
-    hom_menos_40 = homens40(func, mulheres, homens)
-    print(hom_menos_40)
+    homens40(func, mulheres, homens)
+    
