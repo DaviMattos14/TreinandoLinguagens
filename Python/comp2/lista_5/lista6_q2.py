854541fefd85ffb8fff3c14be05698b19106834c
DaviMattos14 <davimattos2012@gmail.com>
2022-10-24 21:41:18 -0300

    Lista 6 - Exceções - Questão 2 - Comp2 2022.2

---
+++ Python/comp2/lista_5/lista6_q2.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    def __init__(self, nome, celular, telefone_fixo, email):
+        if len(nome) < 3: raise Exception('Nome inválido')
+        if len(email) <= 10: raise Exception('E=mail Inválido')
+        if len(str(celular)) <8 or len(str(telefone_fixo)) < 8: raise Exception('Número Inválido')
+        else:
+            self.nome = nome
+            self.celular = celular
+            self.telefone_fixo = telefone_fixo
+            self.email = email
+     
